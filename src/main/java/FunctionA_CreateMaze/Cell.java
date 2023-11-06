6255abc85b7b96e67d57f75a45efac6571d850b2
LOWingYan <wyloan@connect.ust.hk>
2023-11-06 14:02:41 +0800

    [FunctionA] more than 1 possible between entry and exit point

--- src/main/java/FunctionA_CreateMaze/Cell.java
+++ src/main/java/FunctionA_CreateMaze/Cell.java
@@ -6,16 +6,13 @@ public class Cell {
     public int row;
     public int col;
     public int value;
-    boolean[] walls;
     boolean visited;
 
     // Constructor
     public Cell(int row, int col, int value) {
         this.row = row;
         this.col = col;
-        this.walls = new boolean[4]; // Representing top, right, bottom, left walls
         this.visited = false;
-        Arrays.fill(walls, true); // Initialize all walls as closed
         this.value = value;
     }
 
