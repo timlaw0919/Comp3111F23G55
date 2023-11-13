a3398946b53002d395ca82f47b2f425b2d850e2d
timlaw0919 <hnlaw@connect.ust.hk>
2023-11-13 14:57:23 +0800

    [Main] Refactor 1.1

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -14,37 +14,15 @@ import FunctionB_ShortestPath.MazeWithShortestPathGUI;
 import FunctionC_TomCatchJerry.GameMain;
 
 public class Main{
-
-    // Check valid input
-    public static boolean checkValidRowOrCol(int size){
-        return size >= 20;
-    }
-
-    // Check valid size of maze
-    public static int getValidSize(Scanner scanner, String dimension) {
-        int size;
-        while (true) {
-            System.out.println("Please enter the customize " + dimension + " (At least 20)");
-            size = scanner.nextInt();
-            if (checkValidRowOrCol(size)) {
-                break;
-            }
-        }
-        return size;
-    }
-
     public static void main(String[] args) {
 
         int row = 30, col = 30; // Default row and col
 
         int choice;
-        MazeGenerator mazeGenerator = null;
-        AStarAlgorithm aStarObject = null;
         Scanner scanner = new Scanner(System.in);
 
         while (true){
             System.out.println("Menu:");
-            System.out.println("Press 0 to change the maze's size, by default is 30*30");
             System.out.println("Press 1 to test Function A - Generate a maze map");
             System.out.println("Press 2 to test Function B - Generate shortest path");
             System.out.println("Press 3 to test Function C - Play the game");
@@ -53,12 +31,8 @@ public class Main{
             choice = scanner.nextInt(); // Get the user's choice
 
             switch (choice){
-                case 0: // Change maze row and col
-                    row = getValidSize(scanner, "row");
-                    col = getValidSize(scanner, "col");
-                    break;
                 case 1: // Test Function A
-                    mazeGenerator = new MazeGenerator(row, col);
+                    MazeGenerator mazeGenerator = new MazeGenerator(row, col);
                     mazeGenerator.generateMaze();
                     // Output the maze as csv file
                     Cell[][] maze = mazeGenerator.getMaze();
@@ -83,7 +57,7 @@ public class Main{
                             }
                         }
                     }
-                    aStarObject = new AStarAlgorithm(tomLocation, jerryLocation);
+                    AStarAlgorithm aStarObject = new AStarAlgorithm(tomLocation, jerryLocation);
                     CSVOutputForGUI.outputCSVFile(aStarObject.pathGeneratorByAStar(), "maze_map_with_path.csv");
                     MazeWithShortestPathGUI.makeGUI(args);
                     CSVOutput.outputCSVFile(aStarObject.pathGeneratorByAStar(), "path_coordinates.csv");
@@ -96,34 +70,5 @@ public class Main{
                     break;
             }
         }
-
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int rows = 30; // Number of rows in the maze
-//        int cols = 30; // Number of columns in the maze
-//
-//        // Generator the maze
-//        MazeGenerator mazeGenerator = new MazeGenerator(rows, cols);
-//        mazeGenerator.generateMaze();
-//
-//        // Output the maze as csv file
-//        Cell[][] maze = mazeGenerator.getMaze();
-//        outputCSVFile(maze, "maze_map.csv");
-//
-//        // Output GUI maze
-//        MazeGUI.makeGUI(args);
-//
-//        // Find start & end point + Create Object
-//        int[] tomLocation = {mazeGenerator.EntryPoint.row,mazeGenerator.EntryPoint.col};
-//        int[] jerryLocation = {mazeGenerator.ExitPoint.row,mazeGenerator.ExitPoint.col};
-//
-//        AStarAlgorithm obj1 = new AStarAlgorithm(tomLocation, jerryLocation);
-
-//        // GUI with Shortest Path
-//        CSVOutputForGUI.outputCSVFile(obj1.pathGeneratorByAStar(), "maze_map_with_path.csv");
-//        MazeWithShortestPathGUI.makeGUI(args);
-//
-//        // Shortest Path Coordinate output to csv file
-//        CSVOutput.outputCSVFile(obj1.pathGeneratorByAStar(), "path_coordinates.csv");
     }
 }
